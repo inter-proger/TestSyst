204fdedbda1c03905920b9baf3c369f4c3aa48c8
Inter <inter-proger@yandex.ru>
2011-04-04 22:06:16 +0600

    questions and answers add. v1

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -20,6 +20,20 @@ class QuestionsController < ApplicationController
   def show
     @pict=Picture.new
     @answs_count=@quest.answers.count
+    @qtid=@quest.qtype_id
+    if @qtid==9
+      @answs=@quest.answers
+      @arr1=Array.new(10){|i| nil}
+      @arr2=Array.new(10){|i| nil}
+      @answs.each do |elem|
+        if elem.col==1
+          @arr1[elem.right-1]=elem.content
+        else
+          @arr2[elem.right-1]=elem.content
+        end
+      end
+    end
+    
     respond_to do |format|
       format.html # show
       format.xml  { render :xml => @quest }
@@ -43,21 +57,62 @@ class QuestionsController < ApplicationController
 
   def answsadd
     @quest.answers.clear
-    
+    @qtid=@quest.qtype_id
     1.upto(10) do |i|
-      if params[:content][i.to_s]==""
-        break
-      end
-      @answ=@quest.answers.build
-      @answ.content=params[:content][i.to_s]
-      if params[:right]==i.to_s
+        break if params[:content][i.to_s]==""
+        @answ=@quest.answers.build
+        @answ.content=params[:content][i.to_s]
+        if params[:right]==i.to_s
+          @answ.right=1
+        else
+          @answ.right=0
+        end
+        @answ.save
+      end if @qtid==6
+      
+    1.upto(10) do |i|
+        break if params[:content][i.to_s]==""
+        @answ=@quest.answers.build
+        @answ.content=params[:content][i.to_s]
+        if params[:right][i.to_s]=="1"
+          @answ.right=1
+        else
+          @answ.right=0
+        end
+        @answ.save
+      end if @qtid==7
+     1.upto(10) do |i|
+        break if params[:content][i.to_s]==""
+        @answ=@quest.answers.build
+        @answ.content=params[:content][i.to_s]
         @answ.right=1
-      else
-        @answ.right=0
-      end
-      @answ.save
-    end
-    @answs_count=@quest.answers.count
+        @answ.save
+      end if @qtid==8
+      1.upto(10) do |i|
+        break if params[:content][i.to_s]==""
+        @answ=@quest.answers.build
+        @answ.content=params[:content][i.to_s]
+        @answ.right=i
+        @answ.save
+      end if @qtid==10
+
+    1.upto(10) do |i|
+        break if (params[:content1][i.to_s]=="")&&(params[:content2][i.to_s]=="")
+        if (params[:content1][i.to_s]!="")
+          @answ=@quest.answers.build
+          @answ.content=params[:content1][i.to_s]
+          @answ.right=i
+          @answ.col=1
+          @answ.save
+        end
+        if (params[:content2][i.to_s]!="")
+          @answ=@quest.answers.build
+          @answ.content=params[:content2][i.to_s]
+          @answ.right=i
+          @answ.col=2
+          @answ.save
+        end
+      end if @qtid==9
     redirect_to discipline_theme_question_path(@discipline,@theme,@quest)
   end
 
