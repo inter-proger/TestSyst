d8a2d54e048ce0ba790ba4cb4d643f703b7eda65
Inter <inter-proger@yandex.ru>
2011-06-13 22:36:34 +0600

    testsession fix

--- app/controllers/testsessions_controller.rb
+++ app/controllers/testsessions_controller.rb
@@ -3,6 +3,8 @@ class TestsessionsController < ApplicationController
   before_filter :login_required,:only=>[:fastconf,:create,:check,:complete]
   before_filter :prev,:only=>[:show,:complete]
   def new
+    @ts=current_user.testsessions.last
+    redirect_to :action=>:show,:id=>@ts.id.to_s,:num=>"1" and return false if @ts.completed!=1
     @ai='#item1'
     @ts=Testsession.new
   end
