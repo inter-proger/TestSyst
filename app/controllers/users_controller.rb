98c69e97c1fad3f7e8820a767cccee0a47ee7dc1
root <root@smile-desktop.(none)>
2011-06-20 11:37:49 +0400

    2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@
 class UsersController < ApplicationController
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
-  before_filter :login_required, :ecxcept=>[:new,:create]
-  before_filter :admin_required, :ecxcept=>[:new,:create,:show,:edit,:update]
+  before_filter :login_required, :except=>[:new,:create]
+  before_filter :admin_required, :except=>[:new,:create,:show,:edit,:update]
   before_filter :secondmenu
   
 
